cfed0bb872124120e7fc83015f8983da90d33138
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-05-26 14:13:58 -0700

    fix?

--- commands/exp.js
+++ commands/exp.js
@@ -4,13 +4,13 @@ module.exports.run = (client, message, args, config) => {
     const SteamID = require('steamid');
     const fs = require('fs')
     const request = require('request');
-    jp = require('jsonpath-plus');
-    jp.query = function(obj, pathExpression) {
+    const jp = require('jsonpath');
+    /*jp.query = function(obj, pathExpression) {
         return jp({
             json: obj,
             path: pathExpression
         });
-    }
+    }*/
 
     var totalDpm = 0;
     var totalHeals = 0;
