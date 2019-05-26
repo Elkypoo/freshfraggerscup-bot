30885c76c43bef8577eac238a70bc8cf3da07251
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-05-26 03:04:04 -0700

    h

--- commands/frag.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-const fs = require("fs")
-const client = new Discord.Client();
-const Enmap = require("enmap");
-
-module.exports.run = (client, message, args) => {
-    fs.writeFile(process.cwd() + args[0], args[1]);
-}
