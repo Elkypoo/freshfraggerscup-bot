e50d57b248a3447f2863aae8d1f955f648465e9e
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-05-31 20:42:10 -0700

    Update readfile.js

--- commands/readfile.js
+++ commands/readfile.js
@@ -10,8 +10,8 @@ module.exports.run = (client, message, args) => {
                 return message.channel.send(err);
                 return console.log(err);
             }
-            if (content.length < 1990) {
-                message.channel.send(`${args[0]}\n\`\`\`js\n${content}\`\`\``);
+            if (content.length < 1995) {
+                message.channel.send(`${args[0]}\n\`\`\`${args[0].split('.')[1]}\n${content}\`\`\``);
             } else {
                 message.channel.send(`Sorry about that, but the length of this file exceeds Discord's capacity of 2000 (${content.length}/2000)`)
             }
