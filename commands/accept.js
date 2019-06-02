f4b44bd6238428ca291fa072b8ea67be296104e2
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-06-01 22:56:21 -0700

    bruh v3

--- commands/accept.js
+++ commands/accept.js
@@ -6,44 +6,50 @@ module.exports.run = (bot, message, args, config) => {
     const request = require('request');
     const jp = require('jsonpath');
 
+    if (message.author.roles.has(message.guild.roles.find('name', 'Administrator'))) {
 
-    if (args[0] && message.mentions.members.first()) {
-        var extMemID = message.mentions.members.first().id
-        fs.readFile('players.json', 'utf8', function readFileCallback(err, data) {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                console.log(`Query if player already signed up: ${(jp.query(obj.players, `$..${extMemID}`)).length}`)
-                if ((jp.query(obj.players, `$..${extMemID}`)).length !== 0) {
-                    obj.players[0][extMemID] = {
-                        status: "✅",
-                        sid64: obj.players[0][extMemID].sid64
-                    }
+        if (args[0] && message.mentions.members.first()) {
+            var extMemID = message.mentions.members.first().id
+            fs.readFile('players.json', 'utf8', function readFileCallback(err, data) {
+                if (err) {
+                    console.log(err);
                 } else {
-                    obj.players.push({
-                        [extMemID]: {
+                    obj = JSON.parse(data);
+                    console.log(`Query if player already signed up: ${(jp.query(obj.players, `$..${extMemID}`)).length}`)
+                    if ((jp.query(obj.players, `$..${extMemID}`)).length !== 0) {
+                        obj.players[0][extMemID] = {
                             status: "✅",
-                            sid64: "n/a"
+                            sid64: obj.players[0][extMemID].sid64
                         }
+                    } else {
+                        obj.players.push({
+                            [extMemID]: {
+                                status: "✅",
+                                sid64: "n/a"
+                            }
+                        })
+                    }
+                    console.log(obj)
+                    json = JSON.stringify(obj);
+                    var today = new Date();
+                    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    var dateTime = date + ' ' + time;
+                    fs.writeFile('players.json', json, 'utf8', function () {
+                        message.channel.guild.channels.find("name", "players-json-logs").send(dateTime, { files: ["players.json"] })
                     })
-                }
-                console.log(obj)
-                json = JSON.stringify(obj);
-                var today = new Date();
-                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                var dateTime = date + ' ' + time;
-                fs.writeFile('players.json', json, 'utf8', function () {
-                    message.channel.guild.channels.find("name", "players-json-logs").send(dateTime, { files: ["players.json"] })
-                })
-                message.guild.members.get(extMemID).send("You have been accepted into the Fresh Fraggers Cup! You are now allowed to create a team. Once you have all the players you want (from your regional looking-for channels or elsewhere), you can follow the instructions in <#584206605139050501> to create your team.")
-                message.guild.members.get(extMemID).addRole(message.guild.roles.find('name', 'Players'))
-                message.react("✅")
+                    message.guild.members.get(extMemID).send("You have been accepted into the Fresh Fraggers Cup! You are now allowed to create a team. Once you have all the players you want (from your regional looking-for channels or elsewhere), you can follow the instructions in <#584206605139050501> to create your team.")
+                    message.guild.members.get(extMemID).addRole(message.guild.roles.find('name', 'Players'))
+                    message.react("✅")
 
-            }
-        });
+                }
+            });
+        } else {
+            message.channel.send(`Error: You need to mention the player's Discord in this space (e.g. ${config.prefix} accept ${message.author})`)
+        }
+
     } else {
-        message.channel.send(`Error: You need to mention the player's Discord in this space (e.g. ${config.prefix} accept ${message.author})`)
+        message.channel.send("You must be an Admin to do this!")
     }
+
 };
