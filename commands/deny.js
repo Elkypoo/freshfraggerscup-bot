744caecf6143ba89bada98974f03c79495d2be54
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-06-01 23:12:20 -0700

    bruh v3.3

--- commands/deny.js
+++ commands/deny.js
@@ -6,7 +6,7 @@ module.exports.run = (bot, message, args, config) => {
     const request = require('request');
     const jp = require('jsonpath');
 
-    if (message.guild.member(message.member.id).roles.has(message.guild.roles.find('name', 'Administrator'))) {
+    if (message.guild.member(message.member.id).roles.has(message.guild.roles.find('name', 'Administrator').id)) {
 
 
         if (args[0] && message.mentions.members.first()) {
