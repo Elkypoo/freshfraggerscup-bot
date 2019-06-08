e2bb446d206e075aac208643298b5bb64af7114e
Elkypoo <41938455+Elkypoo@users.noreply.github.com>
2019-06-08 12:55:44 -0700

    bruh v8.1

--- bot.js
+++ bot.js
@@ -66,10 +66,12 @@ client.on("message", (message) => {
           if (err) {
             console.log(err);
           } else {
-            message.channel.guild.channels.find("name", "accept-deny").send(`\`\`\`{"${message.author.id}":{
-            "status": "👀",
-            "sid64": "${sid64}"}
-        }\`\`\``)
+            message.channel.guild.channels.find("name", "accept-deny").send(`\`\`\`{
+            "${message.author.id}": {
+              "status": "👀",
+              "sid64": "${sid64}"
+            }
+        },\`\`\``)
             obj = JSON.parse(data);
             console.log(`Query if player already signed up: ${(jp.query(obj.players, `$..${message.author.id}`)).length}`)
             if ((jp.query(obj.players, `$..${message.author.id}`)).length !== 0) {
